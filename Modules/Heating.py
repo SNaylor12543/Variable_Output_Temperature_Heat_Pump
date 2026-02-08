19e859699f65af2fa4f602f35a28e7bed97d864a
Samuel Naylor <sam@grahamnaylor.net>
2026-02-08 16:52:55 +0000

    adding in HVAC calculations

--- Modules/Heating.py
+++ Modules/Heating.py
@@ -73,6 +73,32 @@ class Heat_Pump():
 
         return COP_interp_field(points).reshape(output_temp.shape)
 
+class HVAC():
+    def __init__(self, data_path):
+        self.data_path = data_path
+        # Define column names of outside air temp and heat pump delivery temp
+        self.air_temp_name = "Air temperature(°C)"
+        self.flow_temp_name = "Flow temperature(°C)"
+        
+    def interp_init(self, metric):
+        # initialise interpolation field for different metrics of heat pump performance
+        # x = flow temp
+        # y = air temp
+        # z = desired metric
+        
+        x, y, z = Data.field_data_from_csv(self.data_path, metric, self.air_temp_name, self.flow_temp_name)
+
+        return Interpolator((x, y), z, method='linear', bounds_error=False, fill_value=None)
+    
+    def Calculate_EER(self, output_temp, air_temp, EER_interp_field):
+        # Grab EER at a given output temp and outside temp
+        
+        output_temp, air_temp = np.broadcast_arrays(output_temp, air_temp)
+        
+        points = np.stack([output_temp.ravel(), air_temp.ravel()], axis = -1)
+
+        return EER_interp_field(points).reshape(output_temp.shape)
+
 class HP_Controller():
     def __init__(self, Heat_Pump, Heating_Distribution, max_heat_pump_power, max_HVAC_power):
         self.tool_output_data = "Data/XL-BES-Tool_Output.csv"
@@ -83,8 +109,6 @@ class HP_Controller():
         self.HP = Heat_Pump
         self.HD = Heating_Distribution
                   
-                  
-
         self.COP_interp_field = self.HP.interp_init("COP")
 
     def controller(self):
@@ -108,16 +132,18 @@ class HP_Controller():
         return time_array, heating_demand_array, electricity_demand_array, COP_array, air_temp_array, hydronics_temp_array
 
 class Reverse_HP_Controller():
-    def __init__(self, Heat_Pump, Heating_Distribution, max_heat_pump_power, max_HVAC_power):
+    def __init__(self, Heat_Pump, HVAC, Heating_Distribution, max_heat_pump_power, max_HVAC_power):
         self.tool_output_data = "Data/XL-BES-Tool_Output.csv"
         
         self.max_heat_pump_power = max_heat_pump_power
         self.max_HVAC_power = max_HVAC_power
         
+        self.HVAC = HVAC
         self.HP = Heat_Pump
         self.HD = Heating_Distribution
                   
         self.COP_interp_field = self.HP.interp_init("COP")
+        self.EER_interp_field = self.HVAC.interp_init("EER")
 
     def controller(self):
         # returns electricity demand in kWh!
@@ -126,16 +152,27 @@ class Reverse_HP_Controller():
         air_temp_array = Data.column_from_csv(self.tool_output_data, "External temperture (ºC)")
         # Quick and dirty comparison with GSHP
         # air_temp_array = np.ones(len(air_temp_array)) * 10
+        
         inside_temp_array = Data.column_from_csv(self.tool_output_data, "Indoor_temperature.FF_θair(ºC)")
+
         heating_demand_array = Data.column_from_csv(self.tool_output_data, "Heating_thermal_load(kW)") 
         
         # limit heating demand to max heat pump power
         heating_demand_array = np.clip(heating_demand_array, 0, self.max_heat_pump_power)
         
-        hydronics_temp_array = self.HD.interp_flow_temp_heating(heating_demand_array)
+        heating_hydronics_temp_array = self.HD.interp_flow_temp_heating(heating_demand_array)
         
-        COP_array = self.HP.Calculate_COP(hydronics_temp_array, air_temp_array, self.COP_interp_field)
+        COP_array = self.HP.Calculate_COP(heating_hydronics_temp_array, air_temp_array, self.COP_interp_field)
             
-        electricity_demand_array = np.divide(heating_demand_array, COP_array)
+        heating_electricity_demand_array = np.divide(heating_demand_array, COP_array)
+
+        # Calculating cooling electricity demand
+        cooling_demand_array = Data.column_from_csv(self.tool_output_data, "Cooling_thermal_load(kW)") 
+
+        cooling_hydronics_temp_array = self.HD.interp_flow_temp_cooling(cooling_demand_array)
         
-        return time_array, heating_demand_array, electricity_demand_array, COP_array, air_temp_array, hydronics_temp_array
+        EER_array = self.HVAC.Calculate_EER(cooling_hydronics_temp_array, air_temp_array, self.EER_interp_field)
+            
+        cooling_electricity_demand_array = np.divide(cooling_demand_array, EER_array)
+
+        return time_array, air_temp_array, heating_demand_array, heating_electricity_demand_array, heating_hydronics_temp_array, COP_array, cooling_demand_array, cooling_electricity_demand_array, cooling_hydronics_temp_array, EER_array
